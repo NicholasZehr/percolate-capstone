277cf13ec147fe8dab34d91640102850ddead98c
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-13 14:25:16 -0500

    Added required files for AddReview Component and State Management

--- percolate-capstone/src/components/Routes/routes.js
+++ percolate-capstone/src/components/Routes/routes.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router";
-import AllBusinesses from "../businesses/allBusinesses/AllBusinesses";
-import LoginPage from "../Login";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { AddReview } from '../reviews/AddReview';
+import AllBusinesses from '../businesses/allBusinesses/AllBusinesses';
+import LoginPage from '../Login';
 // import AllBusinesses from "./components/businesses/allBusinesses/AllBusinesses";
 // import LoginPage from "./components/Login";
 
@@ -9,8 +10,9 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/all-business" component={AllBusinesses} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/all-business' component={AllBusinesses} />
+        <Route exact path='/review/add' component={AddReview} />
       </Switch>
     );
   }

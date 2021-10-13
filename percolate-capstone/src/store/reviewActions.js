8dd562a14e6c9c716b38fff542739a35ca715b91
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-13 14:48:38 -0500

    reviewActions addReview things added

---
+++ percolate-capstone/src/store/reviewActions.js
@@ -0,0 +1,25 @@
+import { ADD_REVIEW } from './reviewReducer';
+import { collection, addDoc } from 'firebase/firestore';
+import db from '../firebase';
+
+// ------------------ Actions creators --------------------
+
+export const _addReview = (review) => ({
+  type: ADD_REVIEW,
+  review,
+});
+
+// ------------------ Thunk creators -----------------------
+
+export const addReview = (review) => {
+  return async (dispatch) => {
+    try {
+      const response = await addDoc(collection(db, 'reviews'));
+      dispatch(_addReview(response));
+      console.log('add review response:', response);
+    } catch (error) {
+      console.log('Failed to add review');
+      return;
+    }
+  };
+};
---
+++ percolate-capstone/src/store/reviewReducer.js
@@ -0,0 +1,14 @@
+export const ADD_REVIEW = 'ADD_REVIEW';
+
+// ------------------ Initial State -----------------------
+const initialState = {
+  review: {},
+};
+
+//==================== REDUCER FUNCTION ====================
+export default function businesssesReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

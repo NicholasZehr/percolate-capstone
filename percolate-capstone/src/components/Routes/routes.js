a7d1aeebd740ae845a0b6176c5a2c5cd8edd4d3b
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-14 11:08:10 -0500

    yes

--- percolate-capstone/src/components/Routes/routes.js
+++ percolate-capstone/src/components/Routes/routes.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router';
-import AllBusinesses from '../businesses/allBusinesses/AllBusinesses';
-import LoginPage from '../Login';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router";
+import AllBusinesses from "../businesses/allBusinesses/AllBusinesses";
+import LoginPage from "../Login";
 // import AllBusinesses from "./components/businesses/allBusinesses/AllBusinesses";
 // import LoginPage from "./components/Login";
 
@@ -9,8 +9,8 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route path='/login' component={LoginPage} />
-        <Route path='/all-business' component={AllBusinesses} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/all-business" component={AllBusinesses} />
       </Switch>
     );
   }

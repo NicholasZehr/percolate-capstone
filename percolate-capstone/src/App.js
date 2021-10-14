f4aeee096c4d30c12895e773a708dfa165252020
Greg Schotte <schotte.greg@gmail.com>
2021-10-14 15:46:17 -0400

    reverting to ae062e1

--- percolate-capstone/src/App.js
+++ percolate-capstone/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 // import AllBusinesses from "./components/businesses/allBusinesses/AllBusinesses";
 // import LoginPage from "./components/Login";
-import Routes from "./components/Routes/Routes";
-
-//how to CRUD with firestore: https://firebase.google.com/docs/firestore/manage-data/add-data#web-version-9
+import Routes from "./components/Routes/routes";
 
 function App() {
   return (
--- percolate-capstone/src/store/index.js
+++ percolate-capstone/src/store/index.js
@@ -1,28 +1,26 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import auth from "./auth";
-import { getFirestore, reduxFirestore } from "redux-firestore";
-import db from "../firebase";
-import businessesReducer from "./businessesReducer";
-import singleCoffeeReducer from "./singleCoffee";
+import { applyMiddleware, combineReducers, createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import auth from './auth';
+import { getFirestore, reduxFirestore } from 'redux-firestore';
+import db from '../firebase';
+import businessesReducer from './businessesReducer'
 
 const reducer = combineReducers({
   auth,
-  businesses: businessesReducer,
-  singleCoffee: singleCoffeeReducer,
+  businesses: businessesReducer
 });
 const middleware = composeWithDevTools(
   applyMiddleware(
-    thunkMiddleware.withExtraArgument({ getFirestore }),
+    thunkMiddleware.withExtraArgument({getFirestore }),
     createLogger({ collapsed: true })
   ),
-  reduxFirestore(db)
+  reduxFirestore(db),
 );
 
 //* Create the store
 const store = createStore(reducer, middleware);
 
 export default store;
-export * from "./auth";
+export * from './auth';

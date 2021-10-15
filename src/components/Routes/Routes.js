d6199e7d165c0d9fb963630453bcabf5a89bbfb2
Greg Schotte <schotte.greg@gmail.com>
2021-10-15 12:46:36 -0400

    single coffee renders on page

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,9 +3,10 @@ import { Route, Switch } from "react-router";
 import AllBusinesses from "../businesses/allBusinesses/AllBusinesses";
 import AddReview from "../reviews/AddReview";
 import LoginPage from "../loginSignup/Login";
-import Signup from '../loginSignup/Signup';
-import Business from "../businesses/singleBusiness/singleBusiness"
+import Signup from "../loginSignup/Signup";
+import Business from "../businesses/singleBusiness/singleBusiness";
 import SingleUserPage from "../user/SinglePageUser";
+import SingleCoffee from "../coffee/SingleCoffee";
 // import AllBusinesses from "./components/businesses/allBusinesses/AllBusinesses";
 // import LoginPage from "./components/Login";
 
@@ -13,13 +14,13 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-
-        <Route path='/review/add' component={AddReview} />
+        <Route path="/review/add" component={AddReview} />
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component={Signup} />
         <Route path="/all-business" component={AllBusinesses} />
         <Route path="/singleBusiness" component={Business} />
         <Route path="/users/:id" component={SingleUserPage} />
+        <Route exact path="/coffee/:coffeeId" component={SingleCoffee} />
       </Switch>
     );
   }

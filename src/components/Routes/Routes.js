fbe91c6c64e550ff04ee763243ee2fef76fd9c8a
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-15 11:50:09 -0500

    Commit prior to lunch 15OCT21 with some css troubleshooting

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,23 +3,20 @@ import { Route, Switch } from "react-router";
 import AllBusinesses from "../businesses/allBusinesses/AllBusinesses";
 import AddReview from "../reviews/AddReview";
 import LoginPage from "../loginSignup/Login";
-import Signup from '../loginSignup/Signup';
-import Business from "../businesses/singleBusiness/singleBusiness"
+import Signup from "../loginSignup/Signup";
+import Business from "../businesses/singleBusiness/singleBusiness";
 import SingleUserPage from "../user/SinglePageUser";
-// import AllBusinesses from "./components/businesses/allBusinesses/AllBusinesses";
-// import LoginPage from "./components/Login";
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
-
-        <Route path='/review/add' component={AddReview} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/all-business" component={AllBusinesses} />
-        <Route path="/singleBusiness" component={Business} />
-        <Route path="/users/:id" component={SingleUserPage} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/all-business' component={AllBusinesses} />
+        <Route path='/singleBusiness' component={Business} />
+        <Route path='/users/:id' component={SingleUserPage} />
+        <Route exact path='/review/add' component={AddReview} />
       </Switch>
     );
   }

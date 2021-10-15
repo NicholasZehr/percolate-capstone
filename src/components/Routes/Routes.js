838ebd7ffbd7947cebb023c422c7965701bdf510
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-15 10:43:54 -0500

    This is for the morning PR 15OCT21

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router";
 import AllBusinesses from "../businesses/allBusinesses/AllBusinesses";
-import Business from "../businesses/singleBusiness/singleBusiness"
+import Business from "../businesses/singleBusiness/singleBusiness";
 import LoginPage from "../Login";
-import Signup from '../Signup';
+import AddReview from "../reviews/AddReview";
+import Signup from "../Signup";
 // import AllBusinesses from "./components/businesses/allBusinesses/AllBusinesses";
 // import LoginPage from "./components/Login";
 
@@ -15,6 +16,7 @@ class Routes extends Component {
         <Route path='/signup' component={Signup} />
         <Route path='/all-business' component={AllBusinesses} />
         <Route path='/singleBusiness' component={Business} />
+        <Route path='/review/add' component={AddReview} />
       </Switch>
     );
   }

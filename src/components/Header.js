8094bd90c5b18ae6824d8456c514d6a5a85cead0
Greg Schotte <schotte.greg@gmail.com>
2021-10-16 00:17:58 -0400

    Added alt text to images to get rid of yellow squiggles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { logout } from '../store/auth';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { logout } from "../store/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Header = () => {
   const history = useHistory();
-  const login = getAuth()
-  const [user, setUser] = useState(getAuth().currentUser)
+  const login = getAuth();
+  const [user, setUser] = useState(getAuth().currentUser);
   onAuthStateChanged(login, (u) => {
-    setUser(u)
-  })
+    setUser(u);
+  });
 
   return (
     <div className="header">
       <div className="header-navbar">
-        <div className="oursite" onClick={() => history.push('/home')}>
+        <div className="oursite" onClick={() => history.push("/home")}>
           <span className="brand">Percolate</span>
           <img
             className="logo"
-            src={process.env.PUBLIC_URL + '/logo.png'}
-            onClick={() => history.push('/home')}
+            src={process.env.PUBLIC_URL + "/logo.png"}
+            onClick={() => history.push("/home")}
+            alt="Percolate Logo"
           />
         </div>
 
         <div className="blank">
-          <div className="about" onClick={(_) => history.push('/about')}>
+          <div className="about" onClick={(_) => history.push("/about")}>
             About
           </div>
-          <div className="about" onClick={(_) => history.push('/contact')}>
+          <div className="about" onClick={(_) => history.push("/contact")}>
             <div>Contact Us</div>
           </div>
           <div className="space"></div>
         </div>
         <div className="search">
-          <input className="search-input" placeholder="Search Coffee | location | zip code" />
+          <input
+            className="search-input"
+            placeholder="Search Coffee | location | zip code"
+          />
           <div className="search-label">Search</div>
         </div>
         <div className="blank"></div>
-        <div className="loginBox" onClick={() => history.push(`/users/${user.uid}`)}>
-            <div className="imageBox">
-              <img
-                className="profPic"
-                src={user? user.photoURL:process.env.PUBLIC_URL + '/guest.jpeg'}
-              />
-            </div>
-            <div>
-              <span className='userName'>{user? user.displayName:'Sign in'}</span>
-            </div>
+        <div
+          className="loginBox"
+          onClick={() => history.push(`/users/${user.uid}`)}
+        >
+          <div className="imageBox">
+            <img
+              className="profPic"
+              src={
+                user ? user.photoURL : process.env.PUBLIC_URL + "/guest.jpeg"
+              }
+              alt="User Profile AVI"
+            />
+          </div>
+          <div>
+            <span className="userName">
+              {user ? user.displayName : "Sign in"}
+            </span>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Header;

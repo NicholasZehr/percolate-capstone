6f3098d0fbdc0e487968e68472011c3638d2658a
Jing Lin <linjinglinjing123@gmail.com>
2021-10-18 22:22:23 -0400

    refactor editprofile button

---
+++ src/components/user/EditProfileButton.js
@@ -0,0 +1,157 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser } from "../../store/Actions/usersActions";
+import db from "../../firebase";
+import { doc, setDoc } from "firebase/firestore";
+import {
+  updateProfile,
+  updatePassword,
+} from "firebase/auth";
+
+const EditProfileButton = (props) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const userInfo = {
+      email: evt.target.email.value,
+      displayName: evt.target.firstName.value,
+      lastName: evt.target.lastName.value,
+      password: evt.target.password.value,
+      photoURL: evt.target.photoURL.value,
+      coverURL: evt.target.coverURL.value,
+      favorite: evt.target.favorite.value,
+      coffeeURL: evt.target.coffeeURL.value,
+    };
+    const nonEmptyValue = {};
+    for (const key in userInfo) {
+      if (userInfo[key].length > 0 && key !== "password") {
+        nonEmptyValue[key] = userInfo[key];
+      }
+    }
+
+    //================== update basic info in auth
+
+    updateProfile(props.user, nonEmptyValue, props.user);
+
+    //================== update password in auth
+    if (userInfo.password.length >= 6) {
+      updatePassword(props.user, userInfo.password)
+        .then(() => {
+          // Update successful.
+          console.log("Yes, password changed");
+        })
+        .catch((error) => {
+          // An error ocurred
+          // ...
+          console.log("somthing wrong", error);
+        });
+    }
+    //============== update detail info in firestore data
+    const userRef = doc(db, "Users", props.user.uid);
+    await setDoc(userRef, nonEmptyValue, { merge: true });
+    //==============update redux store user info
+    await dispatch(fetchUser(props.user ? props.user.uid : {}));
+    props.setEdit(!props.edit);
+  };
+  function editPage() {
+    props.setEdit(!props.edit);
+  }
+  return (
+    <>
+      <div className="close" onClick={editPage}></div>
+      <h2>Edit Profile</h2>
+      <form
+        className="signupform"
+        open={false}
+        onSubmit={handleSubmit}
+        name="signup"
+      >
+        <div className="emailBox mod">
+          <span className="formName">Email:</span>
+          <input
+            className="email"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Display Name:</span>
+          <input
+            className="email"
+            name="firstName"
+            type="text"
+            placeholder="Frist Name"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Last Name:</span>
+          <input
+            className="email"
+            name="lastName"
+            placeholder="Last Name"
+            type="text"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Password:</span>
+          <input
+            className="email"
+            name="password"
+            placeholder="Password"
+            type="password"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Profile Picture:</span>
+          <input
+            className="email"
+            name="photoURL"
+            type="text"
+            placeholder="picture URL"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Cover Picture:</span>
+          <input
+            className="email"
+            name="coverURL"
+            placeholder="cover image url"
+            type="text"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Favorite Coffee:</span>
+          <input
+            className="email"
+            name="favorite"
+            type="text"
+            placeholder="Coffee"
+          />
+          <div className="blank3"></div>
+        </div>
+        <div className="emailBox mod">
+          <span className="formName">Coffee Picture:</span>
+          <input
+            className="email"
+            name="coffeeURL"
+            placeholder="favorite coffee url"
+            type="text"
+          />
+          <div className="blank3"></div>
+        </div>
+        <button className="signupPage" name="button1">
+          Save
+        </button>
+      </form>
+    </>
+  );
+};
+export default EditProfileButton
--- src/components/user/SinglePageUser.js
+++ src/components/user/SinglePageUser.js
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
-import { useParams } from 'react-router-dom';
-import { fetchUser } from '../../store/Actions/usersActions';
-import db from '../../firebase';
-import { doc, setDoc } from 'firebase/firestore';
-import {
-  getAuth,
-  onAuthStateChanged,
-  updateProfile,
-  updatePassword,
-} from 'firebase/auth';
-import Modal from 'react-modal';
-Modal.setAppElement('#root');
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { useParams } from "react-router-dom";
+import { fetchUser } from "../../store/Actions/usersActions";
+import db from "../../firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import EditProfileButton from "./EditProfileButton";
+import Modal from "react-modal";
 
-
+Modal.setAppElement("#root");
 
 const SingleUserPage = () => {
-  const {id} = useParams()
+  const { id } = useParams();
   const history = useHistory();
   const dispatch = useDispatch();
   //componentDidMount here
@@ -25,12 +20,11 @@ const SingleUserPage = () => {
   const [user, setUser] = useState(getAuth().currentUser);
   const [edit, setEdit] = useState(false);
   const realUser = useSelector((state) => state.users.user);
-  const [followers, setFollowers] = useState([])
-
-
+  const [followers, setFollowers] = useState([]);
   onAuthStateChanged(auth, (u) => {
     setUser(u);
   });
+
   useEffect(() => {
     let mounted = true;
     async function fetchData() {
@@ -43,161 +37,32 @@ const SingleUserPage = () => {
     return () => {
       mounted = false;
     };
-  }, []);
+  }, [id]);
   useEffect(() => {
-    const list = []
+    const list = [];
+    let mounted = true;
     if (Object.keys(realUser).length > 0) {
       realUser.followers.forEach((element) => {
         list.push(element);
       });
     }
-    setFollowers(list)
-  },[realUser])
+    if (mounted) {
+      setFollowers(list);
+    }
+    return () => {
+      mounted = false;
+    };
+  }, [realUser]);
 
   function editPage() {
     setEdit(!edit);
   }
-  const handleSubmit = async (evt) => {
-    evt.preventDefault();
-    const userInfo = {
-      email: evt.target.email.value,
-      displayName: evt.target.firstName.value,
-      lastName: evt.target.lastName.value,
-      password: evt.target.password.value,
-      photoURL: evt.target.photoURL.value,
-      coverURL: evt.target.coverURL.value,
-      favorite: evt.target.favorite.value,
-      coffeeURL: evt.target.coffeeURL.value,
-    };
-    const nonEmptyValue = {}
-    for (const key in userInfo) {
-      if (userInfo[key].length > 0 && key!=="password") {
-        nonEmptyValue[key] = userInfo[key]
-      }
-    }
 
-    //================== update basic info in auth
-
-    updateProfile(auth.currentUser, nonEmptyValue, user);
-
-    //================== update password in auth
-    if (userInfo.password.length >= 6) {
-      updatePassword(auth.currentUser, userInfo.password)
-        .then(() => {
-          // Update successful.
-          console.log('Yes, password changed');
-        })
-        .catch((error) => {
-          // An error ocurred
-          // ...
-          console.log('somthing wrong');
-        });
-    }
-    console.log(nonEmptyValue)
-    //============== update detail info in firestore data
-    const userRef = doc(db, 'Users', user.uid);
-    await setDoc(userRef, nonEmptyValue, {merge:true});
-    //==============update redux store user info
-    await dispatch(fetchUser(user ? user.uid : {}));
-    setEdit(!edit);
-  };
   return (
     <div className="singleUserPageBox">
       <Modal className="modal" isOpen={edit} onRequestClose={editPage}>
-        <div className="close" onClick={editPage}></div>
-        <h2>Edit Profile</h2>
-        <form
-          className="signupform"
-          open={false}
-          onSubmit={handleSubmit}
-          name="signup"
-        >
-          <div className="emailBox mod">
-            <span className="formName">Email:</span>
-            <input
-              className="email"
-              name="email"
-              type="text"
-              placeholder="Email"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Display Name:</span>
-            <input
-              className="email"
-              name="firstName"
-              type="text"
-              placeholder="Frist Name"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Last Name:</span>
-            <input
-              className="email"
-              name="lastName"
-              placeholder="Last Name"
-              type="text"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Password:</span>
-            <input
-              className="email"
-              name="password"
-              placeholder="Password"
-              type="password"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Profile Picture:</span>
-            <input
-              className="email"
-              name="photoURL"
-              type="text"
-              placeholder="picture URL"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Cover Picture:</span>
-            <input
-              className="email"
-              name="coverURL"
-              placeholder="cover image url"
-              type="text"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Favorite Coffee:</span>
-            <input
-              className="email"
-              name="favorite"
-              type="text"
-              placeholder="Coffee"
-            />
-            <div className="blank3"></div>
-          </div>
-          <div className="emailBox mod">
-            <span className="formName">Coffee Picture:</span>
-            <input
-              className="email"
-              name="coffeeURL"
-              placeholder="favorite coffee url"
-              type="text"
-            />
-            <div className="blank3"></div>
-          </div>
-          <button className="signupPage" name="button1">
-            Save
-          </button>
-        </form>
+        <EditProfileButton edit={edit} setEdit={setEdit} user={user} />
       </Modal>
-
       <div className="profileBox">
         <div className="profileCover">
           <div className="shadow">
@@ -219,8 +84,18 @@ const SingleUserPage = () => {
             <div onClick={editPage} className="editProfileButton">
               Edit Profile
             </div>
+            <div onClick={editPage} className="editProfileButton">
+              Follow
+            </div>
+            <div onClick={editPage} className="editProfileButton">
+              Unfollow
+            </div>
             <h2>
-              {realUser ? (realUser.firstName||realUser.displayName) + " " + realUser.lastName : ""}
+              {realUser
+                ? (realUser.firstName || realUser.displayName) +
+                  " " +
+                  realUser.lastName
+                : ""}
             </h2>
             <hr className="divider" />
             <div className="menu">

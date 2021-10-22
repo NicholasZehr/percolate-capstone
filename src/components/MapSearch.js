09870229ecff65aeee41628435dd2935cd088cf0
Matt Stern <mestern@smu.edu>
2021-10-22 17:11:41 -0400

    algolia geosearch started

--- src/components/MapSearch.js
+++ src/components/MapSearch.js
@@ -1,35 +1,65 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState} from 'react';
 import GoogleMapReact from 'google-map-react';
+import {
+  GeoSearch,
+  Control,
+  Marker,
+} from 'react-instantsearch-dom-maps';
+/*resources for algolia search
+https://www.algolia.com/doc/api-reference/widgets/geo-search/js/
+https://www.algolia.com/doc/guides/managing-results/refine-results/geolocation/
+https://www.algolia.com/doc/guides/managing-results/refine-results/geolocation/how-to/filter-results-around-a-location/?client=javascript*/
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class MapSearch extends Component {
-  static defaultProps = {
-    center: {
-      lat: 40.7128,
-      lng: -73.935242
-    },
-    zoom: 11
+const MapSearch =()=> {
+  let [center, setCenter] = useState({})
+  useEffect(()=>{
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
   };
 
-  render() {
+  function success(pos) {
+    var crd = pos.coords;
+    setCenter({lat: crd.latitude, lng: crd.longitude});
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, options)},[]);
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '50vh' }}>
+      center.lng?(
+      <div id="map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyD9zxNq0hPgKWsXAIdCsBCGyCoszWaRCEk'}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={center}
+          defaultZoom={14}
         >
+          {google => (
+      <GeoSearch google={google}>
+        {({ hits }) => (
+          <div>
+            <Control />
+            {hits.map(hit => (
+              <Marker key={hit.objectID} hit={hit} />
+            ))}
+          </div>
+        )}
+      </GeoSearch>
+    )}
           <AnyReactComponent
             lat={40.7128}
             lng={-74.0060}
             text="NYC"
           />
         </GoogleMapReact>
-      </div>
+      </div>):(<h1>loading location...</h1>)
     );
-  }
 }
 
 export default MapSearch;

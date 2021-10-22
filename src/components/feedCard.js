bdc95fe4a418882ba7c22d7d5fb4d35d2c8a1384
Jing Lin <linjinglinjing123@gmail.com>
2021-10-22 00:38:22 -0400

    changed a lot to make feed card showing proper data

--- src/components/feedCard.js
+++ src/components/feedCard.js
@@ -1,26 +1,59 @@
 import React, { useEffect, useReducer, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { useHistory } from "react-router";
 import { fetchSingleCoffee } from "../store/singleCoffee";
 
 const FeedCard = (props) => {
-  const dispatch = useDispatch();
-  const singleCoffee = useSelector((state) => state.singleCoffee);
-
-  useEffect(() => {
-    if (props.review) {
-      console.log('sheldon',props.review)
-      dispatch(fetchSingleCoffee(props.review.coffeeId));
-    }
-    console.log(singleCoffee);
-  }, []);
+  const history = useHistory();
+  // const dispatch = useDispatch();
+  // const singleCoffee = useSelector((state) => state.singleCoffee);
+  // const [coffee, setCoffee]= useState(singleCoffee)
+  // useEffect(() => {
+  //   if (props.review) {
+  //     console.log("sheldon", singleCoffee);
+  //     dispatch(fetchSingleCoffee(props.review.coffeeId));
+  //   }
+  // }, []);
   //CSS textarea expanding
   const textarea = document.getElementById("txt");
-  if(textarea){textarea.addEventListener("input", function (e) {
-    this.style.height = "auto";
-    this.style.height = this.scrollHeight + "px";
-  });}
-  
+  if (textarea) {
+    textarea.addEventListener("input", function (e) {
+      this.style.height = "auto";
+      this.style.height = this.scrollHeight + "px";
+    });
+  }
+
+  let time = props.review.time
+    ? new Date(props.review.time.seconds * 1000)
+    : "no time";
+  function timeDifference(input) {
+    input *= 1000
+    let msPerMinute = 60 * 1000;
+    let msPerHour = msPerMinute * 60;
+    let msPerDay = msPerHour * 24;
+    let msPerMonth = msPerDay * 30;
+    let msPerYear = msPerDay * 365;
+    let current = Date.now();
+    console.log(current)
+    console.log(input)
+    let elapsed = current - input;
+
+    if (elapsed < msPerMinute) {
+      return Math.round(elapsed / 1000) + " secs ago";
+    } else if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + " mins ago";
+    } else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + " hours ago";
+    } else if (elapsed < msPerMonth) {
+      return "approximately " + Math.round(elapsed / msPerDay) + " days ago";
+    } else if (elapsed < msPerYear) {
+      return (
+        "approximately " + Math.round(elapsed / msPerMonth) + " months ago"
+      );
+    } else {
+      return "approximately " + Math.round(elapsed / msPerYear) + " years ago";
+    }
+  }
   return (
     <>
       <div className="self feeding cardDown">
@@ -34,33 +67,35 @@ const FeedCard = (props) => {
                   ? props.user.photoURL || "/guest.jpeg"
                   : "/guest.jpeg"
               }
+              onClick={(_) => history.push(`/users/${props.user.uid}`)}
             />
-            <div className="username writepost">
-              {props.user ? (
-                <p>
-                  {props.user.displayName + " " + props.loggedInUser.lastName}
-                </p>
-              ) : (
-                "Sign in"
-              )}
-            </div>
+          </div>
+          <div className="nameAndTime">
+            <span className="writepost">{props.user.displayName}: </span>
+            {props.review.time ? (
+              <span className="ago">{`${timeDifference(props.review.time.seconds)}`}</span>
+            ) : (
+              <span className="ago">no time</span>
+            )}
+          </div>
+          <div className="username writepost">
+            <p>{props.review.reviewContent}</p>
           </div>
         </div>
       </div>
-      <div
-        className="self feeding cardUpAdjustment"
-        onClick={(_) => console.log("fdsafdsafdf", singleCoffee)}
-      >
-        <div className="headNPost card">
-          <img
-            className="favCoffee"
-            src={singleCoffee ? singleCoffee.photoUrl : singleCoffee}
-          />
+      <div className="self feeding cardUpAdjustment">
+        <div
+          className="headNPost card"
+          onClick={(_) => history.push(`/coffees/${props.review.coffeeId}`)}
+        >
+          <img className="favCoffee" src={props.review.photoUrl} />
           <div className="coffeeInfo">
-            <p>Roast: {singleCoffee.roast}</p>
-            <p>Brand: {singleCoffee.brandName}</p>
-            <p>Average Rate: {singleCoffee.avgRating}</p>
-            <p>Location: {singleCoffee.roasterCity}</p>
+            <p>Roast: {props.review.roast}</p>
+            <p>Brand: {props.review.brandName}</p>
+            <p>
+              <b>{props.user.displayName}'s </b>Rating: {props.review.rating}/5
+            </p>
+            <p>Location: {props.review.roasterCity}</p>
           </div>
         </div>
       </div>

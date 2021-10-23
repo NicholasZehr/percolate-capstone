53e2051161508324b16060f45c21db2d8e72e49e
Nicholas Zehr <nicholas.zehr@icloud.com>
2021-10-22 22:09:12 -0500

    Refactoring to hold state for likeStatus

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -2,20 +2,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { likeClick } from "../store/reviewActions";
 import { getAuth } from "firebase/auth";
-
 const auth = getAuth();
 class LikeButton extends Component {
   constructor(props) {
     super(props);
-    this.state = { user: auth.currentUser };
+    this.state = { user: auth.currentUser, likeStatus: 0 };
     this.handleLike = this.handleLike.bind(this);
   }
-  handleLike() {
+
+  async componentWillUnmount() {
     const likeClick = this.props.likeClick;
     const id = this.props.id;
     const type = this.props.type;
     const { uid, displayName, photoURL } = this.state.user;
-    likeClick(id, this.props.reviewId, uid, displayName, photoURL, type);
+    if (this.state.likeStatus) {
+      likeClick(id, this.props.reviewId, uid, displayName, photoURL, type);
+    }
+  }
+  handleLike() {
+    if (this.state.likeStatus) {
+      this.setState({ ...this.state, likeStatus: 1 });
+    }
   }
   render() {
     const likeCount = this.props.likeCount;
